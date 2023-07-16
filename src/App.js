d9072b7d86e63ab0435b62f273c0d108891e73fa
daniel endaylalu <endaylaludaniel@gmail.com>
2023-07-16 22:28:03 +0300

    seting up open weather api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [city, setCity] = useState("london");
+  const API_KEY = process.env.REACT_APP_MY_API_KEY;
+
+  useEffect(() => {
+    const weatherGeter = async () => {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
+        );
+        const weatherData = await res.json();
+        setData(weatherData);
+        console.log(weatherData);
+      } catch (err) {
+        throw new Error("error while fetching");
+      }
+    };
+    weatherGeter();
+  }, [city, API_KEY]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Heloo</h1>
+        <form
+          onSubmit={(e) => {
+            console.log(API_KEY);
+            e.preventDefault();
+            const newCity = e.target.city.value;
+            setCity(newCity);
+            data.weather && console.log(data.weather[0].icon);
+          }}
         >
-          Learn React
-        </a>
+          <input type="text" name="city" />
+          <button type="submit">submit</button>
+        </form>
+        {/* {data.weather && (
+          <img
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="Weather icon"
+          />
+        )} */}
+        <p></p>
       </header>
     </div>
   );
