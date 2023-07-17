f5a0c6778134283e525b584f3fe5ca5528c51565
daniel endaylalu <endaylaludaniel@gmail.com>
2023-07-17 14:35:17 +0300

    stablished the error page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import img from "./location.png";
+import eye from "./open-eye.png";
+import drop from "./drop.png";
+import wind from "./wind.png";
+import error from "./warning.png";
 
 function App() {
   const [data, setData] = useState("");
@@ -73,19 +77,73 @@ function App() {
                 <h2>Daily Summery</h2>
                 <p>
                   Now it feels like {(data.main.feels_like - 273.15).toFixed(2)}
-                  &#xB0;, acctuly{(data.main.temp - 273.15).toFixed(2)}&#xB0; ,
+                  &#xB0;, acctuly {(data.main.temp - 273.15).toFixed(2)}&#xB0; ,
                   <br />
-                  its feels there will be a {data.weather[0].description} ,
+                  it feels there will be a {data.weather[0].description} ,
                   <br /> Today the temprature is felt in the range from{" "}
                   {(data.main.temp_max - 273.15).toFixed(2)}&#xB0;, to{" "}
                   {(data.main.temp_min - 273.15).toFixed(2)}&#xB0;
                 </p>
               </div>
+              <div className="wind">
+                <div>
+                  <img src={wind} alt="wind" />
+                  <h6>{data.wind.speed}km/hr</h6>
+                  <h6>Speed</h6>
+                </div>
+                <div>
+                  <img src={drop} alt="humidity" />
+                  <h6>{data.main.humidity}%</h6>
+                  <h6>humidity</h6>
+                </div>
+                <div>
+                  <img src={eye} alt="visibility" />
+                  <h6>{(data.visibility / 1000).toFixed(2)}km</h6>
+                  <h6>visibility</h6>
+                </div>
+              </div>
             </div>
           )
         ) : (
           <div className="container">
-            <p className="err-text">Failed to fetch</p>
+            <div className="top">
+              <img src={img} alt="location" className="location" />
+              <h3 className="country">__</h3>
+            </div>
+            <h1 className="city">Error</h1>
+            <h5 className="date">
+              {weekday} - {date} - {month}
+            </h5>
+            <img src={error} alt="Weather icon" className="icon" />
+            <p className="desc">__</p>
+            <h1 className="temp">null&#xB0;</h1>
+            <div className="summery">
+              <h2>Daily Summery</h2>
+              <p>
+                Now it feels like __ &#xB0;, acctuly __&#xB0; ,
+                <br />
+                it feels there will be a __ ,
+                <br /> Today the temprature is felt in the range from __&#xB0;,
+                to __&#xB0;
+              </p>
+            </div>
+            <div className="wind">
+              <div>
+                <img src={wind} alt="wind" />
+                <h6>__km/hr</h6>
+                <h6>Speed</h6>
+              </div>
+              <div>
+                <img src={drop} alt="humidity" />
+                <h6>__%</h6>
+                <h6>humidity</h6>
+              </div>
+              <div>
+                <img src={eye} alt="visibility" />
+                <h6>__km</h6>
+                <h6>visibility</h6>
+              </div>
+            </div>
           </div>
         )}
       </div>
